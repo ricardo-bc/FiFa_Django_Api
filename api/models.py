ba241180101bdf4780b5ca3b8ed2f9183e18e420
ricardo-bc <55005752+ricardo-bc@users.noreply.github.com>
2021-08-03 22:23:42 -0500

    First commit

---
+++ api/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+
+class gamer(models.Model):
+    class Meta:
+        managed= 'api_gamer'
+    name =  models.CharField(max_length=50, default='')
+    game_position = models.CharField(max_length=50, default='')
+    nationality = models.CharField(max_length=50, default='')
+    team = models.CharField(max_length=50, default='')
+    def __str__(self):
+        return self.name
+        
+
+
---
+++ api/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework import permissions
+# Create your views here.
